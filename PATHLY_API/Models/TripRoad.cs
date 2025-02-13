328646fa85634ebca623ce2cfc7bdcd910b0b631
Ahmed <ahhmedd.ezzat@gmail.com>
2025-02-14 01:20:33 +0200

    Adding Class TripRoad

---
+++ PATHLY_API/Models/TripRoad.cs
@@ -0,0 +1,11 @@
+﻿namespace PATHLY_API.Models
+{
+    public class TripRoad
+    {
+        public int RoadId { get; set; }
+        public int TripId { get; set; }
+
+        public Road Road { get; set; }
+        public Trip Trip{ get; set; }
+    }
+}
